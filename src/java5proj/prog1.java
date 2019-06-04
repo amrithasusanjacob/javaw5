29dc1b43b0676899ce88b1a254fc566ad7a9ea43
753813 <753813@123-PC07.student.Tor.Loc>
2019-06-04 14:18:41 -0400

    func to calc tax

--- src/java5proj/prog1.java
+++ src/java5proj/prog1.java
@@ -1,12 +1,27 @@
-package java5proj;
 
+package java5proj;
+import java.util.*;
 public class prog1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
-		
+		// TODO Auto-generated method stub	
+		Scanner obj=new Scanner(System.in);
+		double price=0.0;
+		System.out.println("Enter the price");
+		price=obj.nextDouble();
+		calctax(price);
+		}
+	/*func that calculate the tax
+	 * func takes 1 param :price
+	 * func o/p the total tax on the price
+	 * o/p to console
+	 */
+	public static void calctax(double p)
+	{
+		final double TAXRATE=0.13;
+		double res=p*TAXRATE;
+		System.out.println("TAX :"+res);
 	}
-
+	
+	
 }
