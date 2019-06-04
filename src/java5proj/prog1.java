45c5c929bc4e69dc47bd00027256c558dc7580f5
753813 <753813@123-PC07.student.Tor.Loc>
2019-06-04 15:37:28 -0400

    prog to get the letter grade

--- src/java5proj/prog1.java
+++ src/java5proj/prog1.java
@@ -1,36 +1,44 @@
 
 package java5proj;
-//import java.util.*;
+
+import java.util.Scanner;
 public class prog1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub	
-		//Scanner obj=new Scanner(System.in);
-		//double price=0.0;
-		//System.out.println("Enter the price");
-		//price=obj.nextDouble();
-		//calctax(price);
-		calcdist(9.8,10);
-		calcdist(1.62,10);
+		// TODO Auto-generated method stub
+		String g=getlettergrade();
+		System.out.println("The grade: "+g);
+	}
+	public static String getlettergrade()
+	{
+		Scanner obj=new Scanner(System.in);
+		int m=0;
+		for(int i=1;i<6;i++)
+		{
+		   System.out.println("Enter the mark"+i+":");
+		    m=m+obj.nextInt();
 		}
-	/*func that calculate the tax
-	 * func takes 1 param :price
-	 * func o/p the total tax on the price
-	 * o/p to console
-	 */
-	/*public static void calctax(double p)
-	{
-		final double TAXRATE=0.13;
-		double res=p*TAXRATE;
-		System.out.println("TAX :"+res);
+		double avg = m/5.0;
+		if((avg>=90)&&(avg<101))
+		{
+		           return "A";
+		}
+		else if((avg>=80)&&(avg<90))
+		{
+		           return "B";
+		}
+		else if((avg>=70)&&(avg<80))
+		{
+		           return "C";
+		}
+		else if((avg>=60)&&(avg<70))
+		{
+		           return "D";
+		}
+		else
+		{
+			return "F";
+		}
 	}
-	/*Cal dist that obj falls down under gravity
-	 * d=0.5*g*t  (g= gravity(9.8m/s^2))
-	 * t=time
-	 */
-	public static void calcdist(double g, int t)
-	{
-		double dis=0.5*g*t;
-		System.out.println("Distance travelled "+dis);
-	}
+	;
 }
